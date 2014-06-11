daf213898a1f0e117aea1be1e2e1547e03c2a151
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2014-06-10 18:05:47 -0700

    bubble_sort_by accepts a block

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -23,4 +23,26 @@ puts bubble_sort(a)
 
 
 
+#This method accepts a block 
+#The block accepts two arguments which are the values that are being compared at that point which are passed via yield.
 
+def bubble_sort_by(arr)
+	swapped = true
+	while swapped
+		swapped = false
+		for i in 0..arr.length-2
+		    y = yield(arr[i],arr[i + 1])
+		    if y == 1
+				arr[i], arr[i + 1] = arr[i + 1], arr[i]
+				swapped = true
+			end
+		end
+	end
+	return arr
+end
+
+array =  bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+	     right <=> left
+         end
+
+puts array
