33f78ed7d442bad3d00ac47a31e96f09018269aa
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2014-06-10 18:09:00 -0700

    fixed formatting

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,19 +2,19 @@
 #This method takes in an array and returns a sorted array
 
 def bubble_sort(arr)
-	swapped = true
-	while swapped
-		swapped = false
+    swapped = true
+    while swapped
+        swapped = false
 
-		for i in 0..arr.length - 2
-			if (arr[i+1] < arr[i])
-				arr[i], arr[i + 1] = arr[i + 1], arr[i]
-				swapped = true
-			end
-		end
+        for i in 0..arr.length - 2
+            if (arr[i+1] < arr[i])
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = true
+            end
+        end
 
-	end
-	return arr
+    end
+    return arr
 
 end
 
@@ -27,22 +27,22 @@ puts bubble_sort(a)
 #The block accepts two arguments which are the values that are being compared at that point which are passed via yield.
 
 def bubble_sort_by(arr)
-	swapped = true
-	while swapped
-		swapped = false
-		for i in 0..arr.length-2
-		    y = yield(arr[i],arr[i + 1])
-		    if y == 1
-				arr[i], arr[i + 1] = arr[i + 1], arr[i]
-				swapped = true
-			end
-		end
-	end
-	return arr
+    swapped = true
+    while swapped
+        swapped = false
+        for i in 0..arr.length-2
+            y = yield(arr[i],arr[i + 1])
+            if y == 1
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = true
+            end
+        end
+    end
+    return arr
 end
 
 array =  bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
-	     right <=> left
+         right <=> left
          end
 
 puts array
